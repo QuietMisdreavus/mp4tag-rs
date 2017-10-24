7ffc05caf216ae6e4dd210706795b261116dcfee
QuietMisdreavus <grey@quietmisdreavus.net>
2017-10-24 11:10:20 -0500

    change name representation, add find_atom

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,57 @@
 extern crate byteorder;
-extern crate smallstring;
 
 use byteorder::{BigEndian, ReadBytesExt, ByteOrder};
-use smallstring::SmallString;
 
+use std::fmt;
 use std::io::{self, Read, Seek, SeekFrom, ErrorKind, Error};
 use std::str::from_utf8;
 
-const CONTAINER_ATOMS: [&'static str; 11] = [
-    "moov", "udta", "mdia", "meta", "ilst",
-    "stbl", "minf", "moof", "traf", "trak",
-    "stsd"
+const CONTAINER_ATOMS: [&'static [u8; 4]; 11] = [
+    b"moov", b"udta", b"mdia", b"meta", b"ilst",
+    b"stbl", b"minf", b"moof", b"traf", b"trak",
+    b"stsd"
 ];
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct Atom {
     pub start: u64,
     pub len: u64,
-    pub name: SmallString<[u8; 4]>,
+    pub name: [u8; 4],
     pub children: Vec<Atom>,
+}
+
+impl fmt::Debug for Atom {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let name: String = if let Ok(name) = from_utf8(&self.name[..]) {
+            name.to_string()
+        } else {
+            let mut name = String::new();
+
+            let slice = if self.name[0] == 0o251 {
+                name.push('©');
+                &self.name[1..]
+            } else {
+                &self.name[..]
+            };
+
+            if let Ok(suffix) = from_utf8(slice) {
+                name.push_str(suffix);
+            } else {
+                for &byte in slice {
+                    name.push_str(&format!("\\x{:x}", byte));
+                }
+            }
+
+            name
+        };
+
+        f.debug_struct("Atom")
+            .field("start", &self.start)
+            .field("len", &self.len)
+            .field("name", &name)
+            .field("children", &self.children)
+            .finish()
+    }
 }
 
 fn read_atom<F: Read + Seek>(file: &mut F) -> io::Result<Option<Atom>> {
@@ -47,20 +80,12 @@ fn read_atom<F: Read + Seek>(file: &mut F) -> io::Result<Option<Atom>> {
         return Err(Error::new(ErrorKind::InvalidData, "invalid atom size"));
     }
 
-    if let Ok(name) = from_utf8(&header[4..]) {
-        ret.name = SmallString::from_str(name);
-    } else {
-        //TODO: custom error enum; also this may not be a valid assumption
-        //return Err(Error::new(ErrorKind::InvalidData, "name was not utf-8"));
+    ret.name = [header[4], header[5], header[6], header[7]];
 
-        //TODO: okay i found one with a non-utf8 name, now what?
-        ret.name = SmallString::from_str("xxxx");
-    }
-
-    if CONTAINER_ATOMS.iter().any(|&cont| cont == ret.name) {
-        if &*ret.name == "meta" {
+    if CONTAINER_ATOMS.iter().any(|&cont| cont == &ret.name) {
+        if &ret.name == b"meta" {
             file.seek(SeekFrom::Current(4))?;
-        } else if &*ret.name == "stsd" {
+        } else if &ret.name == b"stsd" {
             file.seek(SeekFrom::Current(8))?;
         }
 
@@ -90,4 +115,34 @@ pub fn read_atoms<F: Read + Seek>(file: &mut F) -> io::Result<Vec<Atom>> {
     }
 
     Ok(ret)
+}
+
+pub fn find_atom<'a, I: IntoIterator<Item=S>, S: AsRef<[u8]>>(atoms: &'a [Atom], path: I)
+    -> Option<&'a Atom>
+{
+    fn inner<'a, I: IntoIterator<Item=S>, S: AsRef<[u8]>>(atoms: &'a [Atom],
+                                                          needle: Option<S>,
+                                                          path_suffix: I)
+        -> Option<&'a Atom>
+    {
+        if let Some(name) = needle {
+            for a in atoms {
+                if &a.name == name.as_ref() {
+                    let mut iter = path_suffix.into_iter();
+                    let next = iter.next();
+                    if next.is_none() {
+                        return Some(a);
+                    } else {
+                        return inner(&a.children, next, iter);
+                    }
+                }
+            }
+        }
+
+        None
+    }
+    let mut iter = path.into_iter();
+    let first = iter.next();
+
+    inner(atoms, first, iter)
 }
